947be531b40d2ed331a084ea4aa2a6413e19a189
Creek <hebin@buaa.edu.cn>
2020-02-06 12:46:33 +0800

    提交版本0.1.1 将common utils抽取出来，将serviceutil和webutil抽取出来，并将user项目对应的依赖删除，引入service和web的util。

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/RandomPeople.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.user.controller;
+
+import java.util.Random;
+
+public class RandomPeople {
+    public static void main(String[] args) {
+        Random col=new Random();
+        Random row=new Random();
+
+        int lie = col.nextInt(4) + 1;
+        int hang = row.nextInt(8) + 1;
+        System.out.println("请第"+hang+"行，第"+lie+"列的同学回答问题！");
+    }
+}
